209dbd9a433a72572f0a8a719922793802d88be2
Hilal Mufti <hmufti@cs.washington.edu>
2023-10-14 20:03:35 -0700

    fixed LC 238

--- python/problems/238_product_of_array_except_self.py
+++ python/problems/238_product_of_array_except_self.py
@@ -28,13 +28,12 @@ Follow up: Can you solve the problem in O(1) extra space complexity? (The output
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         left: List[int] = [1] * len(nums)
-        right: List[int] = [1] * len(nums)
-        r: int = 1
+        right: int = 1
+        out: List[int] = [1] * len(nums)
 
         for i in range(1, len(nums)):
-            left[i] = nums[i - 1] * left[i - 1]
-        for i in range(len(nums) - 2, -1, -1):
-            right[i] = nums[i + 1] * right[i + 1]
-        for i in range(len(nums)):
-            left[i] = left[i] * right[i]
-        return left
+            left[i] = left[i - 1] * nums[i - 1]
+        for i in range(len(nums) - 1, -1, -1):
+            out[i] = left[i] * right
+            right *= nums[i]
+        return out
