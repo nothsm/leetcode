1b29443fb1ace1fb2ea02f0facf965a7f8bf986d
Hilal Mufti <hmufti@cs.washington.edu>
2023-10-24 12:39:02 -0700

    simplified

--- python/problems/121_best_time_to_buy_sell_stock.py
+++ python/problems/121_best_time_to_buy_sell_stock.py
@@ -37,9 +37,10 @@ Space Complexity: O(1)
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        ps = prices
         max_profit: int = 0
-        min_price: int = prices[0]
-        for i in range(1, len(prices)):
-            max_profit = max(prices[i] - min_price, max_profit)
-            min_price = min(prices[i], min_price)
+        min_price: int = ps[0]
+        for i in range(1, len(ps)):
+            max_profit = max(ps[i] - min_price, max_profit)
+            min_price = min(ps[i], min_price)
         return max_profit
