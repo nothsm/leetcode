24b293dd20eaab8467c0b390fd996fa38e77fb78
Hilal Mufti <hmufti@cs.washington.edu>
2023-12-02 19:36:42 -0800

    42. Group Anagrams in Racket

---
+++ racket/problems/42-group-anagarms.rkt
@@ -0,0 +1,9 @@
+(define (anagram str)
+    (foldl 
+        (lambda (char ht) (hash-update ht char add1 0))
+        #hash()
+        (string->list str)))
+
+(define (group-anagrams strs)
+  (group-by anagram strs))
+  
