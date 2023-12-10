8ba7cb190ba3320216bb13bd56a3d4d62592c757
Hilal Mufti <hmufti@cs.washington.edu>
2023-12-10 12:59:01 -0800

    +66

--- racket/problems/66-plus-one.rkt
+++ racket/problems/66-plus-one.rkt
@@ -1,20 +1,29 @@
 #lang racket
 
-(define (list-add1 lst)
-  (match lst
-    [(cons x xs) (cons (add1 x) xs)]))
+; Approach 1
+(define (plus-one-recursive digits)
+  (define (go digits)
+    (match digits
+      ['() '(1)]
+      [(cons 9 xs) (cons 0 (go xs))]
+      [(cons x xs) (cons (add1 x) xs)]))
+  (reverse (go (reverse digits))))
+
+
+; Approach 2
+(define (list->number lst)
+  (foldl 
+    (lambda (x acc)
+      (+ (* 10 acc) x))
+    0
+    lst))
+
+(define (number->list n)
+  (define (go n acc)
+    (cond
+      [(< n 10) (cons n acc)]
+      [else (go (quotient n 10) (cons (modulo n 10) acc))]))
+  (go n '()))
 
 (define (plus-one digits)
-  (define (plus-one-helper digits acc) 
-    (match digits
-      [(list x) 
-        (match x
-          [10 (append (list 1 0) acc)]
-          [_ (cons x acc)])]
-      [(cons x xs) 
-        (match x
-          [10 (plus-one-helper (list-add1 xs) (cons 0 acc))]
-          [_ (append (reverse digits) acc)])]))
-  (match (car (reverse digits))
-    [9 (plus-one-helper (list-add1 (reverse digits)) '())]
-    [_ (reverse (list-add1 (reverse digits)))]))
+  (number->list (add1 (list->number digits))))
