559c91f912d706d91be7ef7514fd4e0d429ca62f
Hilal Mufti <hmufti@cs.washington.edu>
2023-12-16 14:11:23 -0800

    +42

--- racket/problems/42-trapping-rain-water.rkt
+++ racket/problems/42-trapping-rain-water.rkt
@@ -1,1 +1,17 @@
 #lang racket
+
+(define (vref vec i)
+    (vector-ref vec i))
+
+; TODO: refactor w anaphoric macros and higher order functions
+(define (max-area height)
+  (define hs (list->vector height))
+  (define (go left right max-ar)
+    (if (>= left right) 
+        max-ar
+        (let* ([l (vref hs left)]
+               [r (vref hs right)])
+            (cond
+                [(< l r) (go (add1 left) right (max max-ar (* (min l r) (- right left))))]
+                [(>= l r) (go left (sub1 right) (max max-ar (* (min l r) (- right left))))]))))  
+  (go 0 (sub1 (vector-length hs)) -1))
