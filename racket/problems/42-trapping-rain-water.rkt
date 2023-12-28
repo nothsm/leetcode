0a2bdfd0e057d4a33776b2663fcedf0b043f8029
Hilal Mufti <hmufti@cs.washington.edu>
2023-12-27 20:32:10 -0800

    +42

--- racket/problems/42-trapping-rain-water.rkt
+++ racket/problems/42-trapping-rain-water.rkt
@@ -1,17 +1,29 @@
 #lang racket
 
-(define (vref vec i)
-    (vector-ref vec i))
-
-; TODO: refactor w anaphoric macros and higher order functions
-(define (max-area height)
-  (define hs (list->vector height))
-  (define (go left right max-ar)
-    (if (>= left right) 
-        max-ar
-        (let* ([l (vref hs left)]
-               [r (vref hs right)])
-            (cond
-                [(< l r) (go (add1 left) right (max max-ar (* (min l r) (- right left))))]
-                [(>= l r) (go left (sub1 right) (max max-ar (* (min l r) (- right left))))]))))  
-  (go 0 (sub1 (vector-length hs)) -1))
+(define (trap height)
+    (define H (list->vector height))
+    
+    (define (go water left right L R)
+        (cond [(>= left right) water]
+              [else 
+                (cond [(<= L R) (go (+ water 
+                                       (max (- (min L R)
+                                               (vector-ref H (add1 left)))
+                                            0))
+                                    (add1 left) 
+                                    right
+                                    (max L (vector-ref H (add1 left)))
+                                    R)]
+                      [else (go (+ water 
+                                   (max (- (min L R) 
+                                           (vector-ref H (sub1 right)))
+                                        0))
+                                   left 
+                                   (sub1 right)
+                                   L
+                                   (max R (vector-ref H (sub1 right))))])]))
+    (go 0 
+        0 
+        (sub1 (length height)) 
+        (vector-ref H 0) 
+        (vector-ref H (sub1 (length height)))))
