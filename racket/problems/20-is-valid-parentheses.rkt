6dbc4ab667239ad8b5a16c2d6c4720473deaf870
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-01 18:30:24 -0800

    +20

--- racket/problems/20-is-valid-parentheses.rkt
+++ racket/problems/20-is-valid-parentheses.rkt
@@ -1,18 +1,12 @@
 #lang racket
 
 (define (is-valid s)
-    (define (valid? open close) ; returns true if open and close form "()" or "{}" or "[]"
-        (or (and (equal? open #\()  (equal? close #\))) 
-            (and (equal? open #\{)  (equal? close #\})) 
-            (and (equal? open #\[)  (equal? close #\]))))
-    (define (helper parens unmatched all-valid?)
-        (match parens
-            ['() (and all-valid? (empty? unmatched))] ; if unmatched is not empty, then we have unmatched parentheses
-            [(cons p ps) 
-                (match p 
-                    [(or #\( #\{ #\[) (helper ps (cons p unmatched) all-valid?)] ; if p is open paren, then we have one more unmatched paren
-                    [(or #\) #\} #\]) ; if p is closing paren: 
-                        (match unmatched
-                            ['() #f] ; if there's no unmatched parentheses, s is not valid
-                            [(cons s ss) (helper ps ss (and all-valid? (valid? s p)))])])])) ; otherwise check if the next unmatched paren matches s
-    (helper (string->list s) '() #t))
+    (let loop ([lst (string->list s)] [ps '()])
+        (match lst
+            ['() (empty? ps)]
+            [(cons #\( ss) (loop ss (cons #\( ps))]
+            [(cons #\{ ss) (loop ss (cons #\{ ps))]
+            [(cons #\[ ss) (loop ss (cons #\[ ps))]
+            [(cons #\) ss) (and (not (empty? ps)) (equal? (car ps) #\() (loop ss (cdr ps)))]
+            [(cons #\} ss) (and (not (empty? ps)) (equal? (car ps) #\{) (loop ss (cdr ps)))]
+            [(cons #\] ss) (and (not (empty? ps)) (equal? (car ps) #\[) (loop ss (cdr ps)))])))
