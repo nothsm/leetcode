7c09b3ccb7f936ad732c74c9b7aa4847d8ba9969
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-02 13:22:40 -0800

    +169

--- racket/problems/169-majority-element.rkt
+++ racket/problems/169-majority-element.rkt
@@ -4,5 +4,6 @@
     (let loop ([lst nums] [res -1] [ct 0])
         (match lst
             ['() res]
-            [(cons x xs) (cond [(zero? ct) (loop xs x (add1 ct))]
-                               [else (loop xs res ((if (= x res) add1 sub1) ct))])])))
+            [(cons x xs) #:when (zero? ct) (loop xs x (add1 ct))]
+            [(cons x xs) #:when (= x res) (loop xs res (add1 ct))]
+            [(cons x xs) (loop xs res (sub1 ct))])))
