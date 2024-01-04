10b3815ba41e87f2eaaea80bd573a0cd803466c7
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-04 07:34:25 -0800

    +347

--- racket/problems/347-top-k-freq-elements.rkt
+++ racket/problems/347-top-k-freq-elements.rkt
@@ -1,12 +1,14 @@
 #lang racket
 
-(define (sorted-dict dict)
-    (sort (hash->list dict) (lambda (p1 p2) (>= (cdr p1) (cdr p2)))))
-
 (define (top-k-frequent nums k)
-    (define (top-k-frequent-acc nums counts)
-        (match nums
-            ['() (take (dict-keys (sorted-dict counts)) k)]
-            [(cons x xs) (top-k-frequent-acc xs (hash-update counts x add1 0))] ))
-    (if (= k (length nums)) nums
-        (top-k-frequent-acc nums #hash())))
+    (define cts (foldl (lambda (x a) (hash-update a x add1 0))
+                       (hash)
+                       nums))
+    (define buckets (foldl (lambda (x a) (hash-update a (hash-ref cts x) (curry cons x) '()))
+                           (hash)
+                           (remove-duplicates nums)))
+    (define sorted (foldl (lambda (ct a) (let ([ns (hash-ref buckets ct #f)])
+                                       (if ns (append a ns) a)))
+                          '()
+                          (inclusive-range (length nums) 1 -1)))
+    (take sorted k))
