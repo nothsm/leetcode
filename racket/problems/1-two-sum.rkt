3d775c909ba89a85b9d408a66daab9007b957061
Hilal Mufti <hmufti@cs.washington.edu>
2024-01-07 17:47:19 -0800

    +1

--- racket/problems/1-two-sum.rkt
+++ racket/problems/1-two-sum.rkt
@@ -1,11 +1,8 @@
 #lang racket
 
 (define (two-sum nums target)
-  (define idx (for/hash ([x nums] [i (in-naturals)])
-                (values x i)))
-
-  (let loop ([lst nums] [left 0])
-    (let* ([want (- target (car lst))] 
-           [right (hash-ref idx want #f)])
-        (cond [(and right (< left right)) (list left right)]
-              [else (loop (cdr lst) (add1 left))]))))
+  (define idx (make-immutable-hash (map cons nums (range (length nums)))))
+  (let loop ([l nums] [i 0])
+    (match l
+      [(cons x xs) #:do [(define c (- target x))] #:when (and (hash-has-key? idx c) (< i (hash-ref idx c))) (list i (hash-ref idx c))]
+      [(cons x xs) (loop xs (add1 i))])))
