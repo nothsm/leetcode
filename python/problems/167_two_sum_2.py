b55c059e02147839ae2766541b3d08b4af2dc6cf
Hilal Mufti <hmufti@cs.washington.edu>
2025-01-16 23:00:02 -0800

    +167

--- python/problems/167_two_sum_2.py
+++ python/problems/167_two_sum_2.py
@@ -1,15 +1,13 @@
-from typing import List
-
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        left: int = 0
-        right: int = len(numbers) - 1
-        while left < right:
-            s: int = numbers[left] + numbers[right]
-            if s < target:
-                left += 1
-            elif s > target:
-                right -= 1
-            else:
-                return [(left + 1), (right + 1)]
-        return []
+    def twoSum(self, xs: List[int], t: int) -> List[int]:
+        l = 0
+        r = len(xs) - 1
+        while xs[l] + xs[r] != t:
+            if xs[l] + xs[r] < t:
+                l += 1
+                r = r
+            elif xs[l] + xs[r] > t:
+                l = l
+                r -= 1
+        return [l + 1, r + 1]
+        
