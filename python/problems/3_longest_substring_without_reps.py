beb5b276818520a98f627a66038769974037d861
Hilal Mufti <hmufti@cs.washington.edu>
2025-01-22 01:32:28 -0800

    +3

--- python/problems/3_longest_substring_without_reps.py
+++ python/problems/3_longest_substring_without_reps.py
@@ -1,10 +1,28 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        ids, l, max_len = {}, 0, 0
-        for r in range(len(s)):
-            next_in_window: bool = s[r] in ids and l <= ids[s[r]]
-            if next_in_window:
-                l = ids[s[r]] + 1
-            max_len = max(max_len, (r - l) + 1)
-            ids[s[r]] = r
-        return max_len
+    def lengthOfLongestSubstring(self, xs: str) -> int:
+        if not xs:
+            return 0
+        else:
+            l = 0
+            r = 1
+            cs = {xs[0]: 1}
+            acc_len = 1
+            acc = 1
+            while r < len(xs):
+                if not cs.get(xs[r], 0):
+                    cs[xs[r]] = cs.get(xs[r], 0) + 1
+
+                    l = l
+                    r += 1
+                    acc_len += 1
+                    acc = max(acc_len, acc)
+                else:
+                    cs[xs[l]] -= 1
+
+                    l += 1
+                    r = r
+                    acc_len -= 1
+                    acc = max(acc_len, acc)
+            return acc
+                
+        
