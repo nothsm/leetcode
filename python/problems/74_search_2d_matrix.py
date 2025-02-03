1d431924453ab2adcb0ac1062f7923be9e0c1455
Hilal Mufti <hmufti@cs.washington.edu>
2025-02-02 19:48:32 -0800

    +74

--- python/problems/74_search_2d_matrix.py
+++ python/problems/74_search_2d_matrix.py
@@ -1,47 +1,32 @@
-from functools import partial, reduce
-import operator as op
-
-def shape(xss):
-    return len(xss), len(xss[0])
-
-def size(xss):
-    return reduce(op.mul, shape(xss))
-
-def idxs(axis, i):
-    r, c = axis
-    return i // c, i % c
-
-def median(lo, hi):
-    return ((hi - lo) // 2) + lo
-
-def is_incr(xs):
-    for i in range(1, len(xs)):
-        if xs[i] <= xs[i-1]:
-            return False
-    return True
-
-def bound(lo, hi, t, otherwise):
-    return (lo if lo < t else otherwise), (hi if hi > t else otherwise)
-
-def search(xss, t):
-
-    idxs_xss = partial(idxs, shape(xss))
-
-    def get(i):
-        r, c = idxs_xss(i)
-        return xss[r][c]
-
-    def bound(lo, hi, med):
-        return lo if t <= get(med) else med, hi if t >= get(med) else med
-
-    lo, hi = 0, size(xss) - 1
-    med = median(lo, hi)
-    while t not in {get(lo), get(med), get(hi)} and is_incr([lo, med, hi]):
-        lo, hi = bound(lo, hi, med)
-        med = median(lo, hi)
-
-    return t in {get(i) for i in [lo, med, hi]}
-
 class Solution:
     def searchMatrix(self, xss: List[List[int]], t: int) -> bool:
-        return search(xss, t)
+        def idxs(i):
+            return i // len(xss[0]), i % len(xss[0])
+
+        def go(l, r):
+            il, jl = idxs(l)
+            ir, jr = idxs(r)
+            
+            xl = xss[il][jl]
+            xr = xss[ir][jr]
+
+            im, jm = idxs((l + r) // 2)
+            xm = xss[im][jm]
+            if xl == t:
+                return True
+            elif xr == t:
+                return True
+            elif r - l - 1 == 0:
+                return False
+            elif t <= xm:
+                return go(l, (l + r) // 2)
+            else:
+                return go((l + r) // 2, r)
+
+        match xss:
+            case []:
+                return False
+            case [[x]]:
+                return x == t
+            case _:
+                return go(0, len(xss) * len(xss[0]) - 1)
