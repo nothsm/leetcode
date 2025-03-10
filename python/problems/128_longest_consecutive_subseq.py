80c1b3d9821888d485ac293b0baa342f83a0efda
Hilal Mufti <hmufti@cs.washington.edu>
2025-03-09 19:08:02 -0700

    +128

--- python/problems/128_longest_consecutive_subseq.py
+++ python/problems/128_longest_consecutive_subseq.py
@@ -1,15 +1,10 @@
-def longest_consec(xs):
-  def is_starting(x):
-    return x - 1 not in xs
-
-  def count_seq(x):
-    y = x
-    while y in xs:
-      y += 1
-    return y - x
-
-  return max(map(count_seq, filter(is_starting, xs)), default=0)
-
 class Solution:
     def longestConsecutive(self, xs: List[int]) -> int:
-        return longest_consec(set(xs))
+        mp = {}
+        acc = 0
+        for x in set(xs):
+            mp[x] = mp.get(x - 1, 0) + 1 + mp.get(x + 1, 0)
+            mp[x - mp.get(x - 1, 0)] = mp[x]
+            mp[x + mp.get(x + 1, 0)] = mp[x]
+            acc = max(mp[x], acc)
+        return acc
