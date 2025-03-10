7a158dd66bf43326920253c9db17bf32d42758d4
Hilal Mufti <hmufti@cs.washington.edu>
2025-03-09 19:02:07 -0700

    +659

--- python/problems/659_encode_decode_strs.py
+++ python/problems/659_encode_decode_strs.py
@@ -1,5 +1,32 @@
-def encode(strs):
-        return ' '.join(strs)
+def encode(ss):
+    return "".join([f"{len(s)}#" + s for s in ss])
 
-def decode(str):
-    return str.split(' ')
+
+def decode(s):
+    i = 0
+    ss = []
+    while i < len(s):
+        if s[i].isdigit():
+            j = i
+            while i < len(s) and s[i].isdigit():
+                i += 1
+            n = int(s[j:i])
+
+            assert s[i] == "#"
+            i += 1
+
+            ss.append(s[i:i+n])
+            i += n
+
+            assert i == len(s) or s[i].isdigit()
+    return ss
+
+
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        return encode(strs)
+
+    def decode(self, s: str) -> List[str]:
+        return decode(s)
