aa2a302b81ae58f69b9cf51d0517504371b8ee7f
Hilal Mufti <hmufti@cs.washington.edu>
2025-03-13 02:27:59 -0700

    +567

--- python/problems/567_permutation_in_string.py
+++ python/problems/567_permutation_in_string.py
@@ -1,22 +1,36 @@
-from typing import Dict
+def frequencies(s):
+    mp = {}
+    for c in s:
+        mp[c] = mp.get(c, 0) + 1
+    return mp
 
-def counts(s: str) -> Dict[str, int]:
-    cs = {}
-    for c in s: 
-        cs[c] = cs.get(c, 0) + 1
-    return cs 
 
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        left = 0
-        counts1 = counts(s1)
-        counts2 = counts(s2[0: len(s1) - 1])
-        for c in s2[len(s1)-1:]:
-            counts2[c] = counts2.get(c , 0) + 1
-            if counts1 == counts2: 
-                return True
-            counts2[s2[left]] -= 1
-            if counts2[s2[left]] == 0: 
-                del counts2[s2[left]]
-            left += 1
+        fs = frequencies(s1)
+        rem = len(s1)
+        l = 0
+        r = 0
+        while r < len(s2):
+            if r - l + 1 < len(s1):
+                if s2[r] in fs:
+                    fs[s2[r]] -= 1
+                    if fs[s2[r]] >= 0:
+                        rem -= 1
+                r += 1
+                if rem <= 0: return True
+            else:
+                if s2[r] in fs:
+                    fs[s2[r]] -= 1
+                    if fs[s2[r]] >= 0:
+                        rem -= 1
+                r += 1
+                if rem <= 0: return True
+                if s2[l] in fs:
+                    fs[s2[l]] += 1
+                    if fs[s2[l]] > 0:
+                        rem += 1
+                l += 1
+            if rem <= 0: return True
         return False
+        
