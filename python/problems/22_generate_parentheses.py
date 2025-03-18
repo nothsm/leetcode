eff32eee1737e66abbb1ad57ba20bef69c1fae2c
Hilal Mufti <hmufti@cs.washington.edu>
2025-03-18 02:53:25 -0700

    +22

--- python/problems/22_generate_parentheses.py
+++ python/problems/22_generate_parentheses.py
@@ -1,13 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        ps = []
-        def gen(l, r, acc):
+        ps = set()
+        def go(l, r, acc):
             if l == 0 and r == 0:
-                ps.append(acc)
-            else:
-                if l > 0:
-                    gen(l - 1, r + 1, acc + "(")
-                if r > 0:
-                    gen(l, r - 1, acc + ")")
-        gen(n, 0, "")
-        return ps
+                ps.add(acc)
+            elif l == 0:
+                go(l, r - 1, acc + ")")
+            elif l < r:
+                go(l - 1, r, acc + "(")
+                go(l, r - 1, acc + ")")
+            elif r >= l:
+                go(l - 1, r, acc + "(")
+        go(n, n, "")
+        return list(ps)
+        
