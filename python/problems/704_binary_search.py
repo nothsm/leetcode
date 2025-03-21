6b08992400ea3b898b789f0f53607e3b7716e3ae
Hilal Mufti <hmufti@cs.washington.edu>
2025-03-21 01:25:46 -0700

    +704

--- python/problems/704_binary_search.py
+++ python/problems/704_binary_search.py
@@ -1,20 +1,31 @@
 class Solution:
     def search(self, xs: List[int], t: int) -> int:
-        def go(l, r):
-            if xs[l] == t:
-                return l
-            elif xs[r] == t:
-                return r
-            elif r - l - 1 == 0: # window size = r - l - 1
-                return -1
-            elif t <= xs[(l + r) // 2]:
-                return go(l, (l + r) // 2)
-            else:
-                return go((l + r) // 2, r)
         match xs:
             case []:
                 return -1
             case [x]:
                 return 0 if x == t else -1
+            case [x, y]:
+                if x == t:
+                    return 0
+                elif y == t:
+                    return 1
+                else:
+                    return -1
             case _:
-                return go(0, len(xs) - 1)
+                l = 0
+                r = len(xs) - 1
+                while r - l > 1:
+                    m = (r + l) // 2
+                    if xs[l] == t:
+                        return l
+                    elif xs[m] == t:
+                        return m
+                    elif xs[r] == t:
+                        return r
+                    elif t < xs[m]:
+                        r = m
+                    elif xs[m] < t:
+                        l = m
+                return -1
+        
