24c3ab3f87151ced578fe42760e5a95549276f32
Hilal Mufti <hmufti@cs.washington.edu>
2025-04-07 15:47:46 -0700

    +217

--- rust/217_contains_duplicate.rs
+++ rust/217_contains_duplicate.rs
@@ -1,18 +1,16 @@
 use std::collections::HashSet;
+use std::hash::Hash;
+
+fn count_unique<I, T>(xs: I) -> usize
+where
+  I: Iterator<Item = T>,
+  T: Eq + Hash
+{
+  xs.collect::<HashSet<_>>().len()
+}
 
 impl Solution {
     pub fn contains_duplicate(xs: Vec<i32>) -> bool {
-        fn go(xs: &[i32], mut acc: HashSet<i32>) -> bool {
-            if xs.len() == 0 {
-                false
-            } else if acc.contains(&xs[0]) {
-                true
-            } else {
-                go(&xs[1..], 
-                   {acc.insert(xs[0]); 
-                    acc})
-            }
-        }
-        go(&xs[..], HashSet::new())
+        count_unique(xs.iter()) != xs.len()
     }
 }
