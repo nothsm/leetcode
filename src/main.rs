58155cd8834c73cc2b5856d178547acff9f37984
Hilal Mufti <hmufti@cs.washington.edu>
2025-07-16 14:38:57 -0700

    +228A

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod problem_1030A;
 mod problem_136A;
-mod problem_200B;
-mod problem_266B;
+mod problem_228A;
 mod problem_344A;
 mod problem_467A;
 mod problem_486A;
+mod problem_200B;
+mod problem_266B;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/problem_228A.rs
@@ -0,0 +1,51 @@
+use std::io;
+
+fn uniq(xs: &Vec<i32>) -> Vec<i32> {
+    let mut i = 0;
+    let mut acc = vec![];
+
+    // Inv: acc is the uniq elements of [xs[0], ..., xs[i - 1]]
+    while i < xs.len() {
+        if !acc.contains(&xs[i]) {
+            acc.push(xs[i]);
+        }
+        i += 1;
+    }
+    acc
+}
+
+fn eval(xs: &Vec<i32>) -> i32 {
+    let uniqs = uniq(xs);
+    assert!(xs.len() >= uniqs.len());
+
+    let diff = (xs.len() - uniqs.len()) as i32;
+    assert!(diff >= 0);
+
+    diff
+}
+
+fn read() -> Vec<i32> {
+
+    let mut s = String::new();
+    io::stdin().read_line(&mut s).expect("couldnt read input");
+
+    let words: Vec<&str> = s.trim().split(' ').collect();
+    // assert!((words.len() * 2) - 1 == s.len());
+
+    let mut i = 0;
+    let mut acc = vec![];
+    // Inv: acc is the ints of [words[0], ..., words[i - 1]]
+    while i < words.len() {
+        acc.push(words[i].parse().expect("couldnt parse int"));
+        i += 1;
+    }
+    assert!(i == words.len());
+    assert!(words.len() == acc.len());
+    acc
+}
+
+fn main() {
+    let xs = read();
+    let horseshoes = eval(&xs);
+    println!("{}", horseshoes);
+}
